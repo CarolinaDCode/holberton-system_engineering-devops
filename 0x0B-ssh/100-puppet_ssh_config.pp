ca5c149a58a6345aa942f640a6d47cba4a8ac4ca
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-01-04 21:56:12 -0500

    configuration ssh_config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Modifying the ssh_config file, to configure the private key
 # in ~ /.ssh /school and for password authentication.
 file_line { 'Turn off passwd auth':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+  line   => '    IdentityFile ~/.ssh/school',
 }
