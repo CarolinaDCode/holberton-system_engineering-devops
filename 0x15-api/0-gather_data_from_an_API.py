bd1228727a7a9e3c413b793122cbb412c8d9e82d
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-01-31 20:31:18 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
         total_task += 1
         if task.get('completed'):
             task_completed += 1
-            
-    print('Employee {} is done  with tasks({:d}/{:d}):'.format(name_user, task_completed, total_task))
+    print('Employee {} is done  with tasks({:d}/{:d}):'
+          .format(name_user, task_completed, total_task))
 
     for task in todos:
         if task.get('completed'):
-            print ('\t ' +task['title'])
-        # print("\t {}".format(task.get['title']))
+            print('\t ' + task['title'])
+            # print("\t {}".format(task.get['title']))
