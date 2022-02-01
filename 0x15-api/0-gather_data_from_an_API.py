89897ab1d3f5309fe6c5f5cf3f345b2c306f2f55
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-01-31 20:28:00 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,13 +8,18 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(argv[1])).json()
     todos = requests.get(url + "users/{}/todos".format(argv[1])).json()
+    name_user = user['name']
+    task_completed = 0
+    total_task = 0
 
-    completed = []
     for task in todos:
-        if task.get("completed"):
-            completed.append(task.get("title"))
-    print("Employee {} is done  with tasks({}/{}):"
-          .format(user.get("name"), len(completed), len(todos)))
+        total_task += 1
+        if task.get('completed'):
+            task_completed += 1
+            
+    print('Employee {} is done  with tasks({:d}/{:d}):'.format(name_user, task_completed, total_task))
 
-    for task_done in completed:
-        print("\t {}".format(task_done))
+    for task in todos:
+        if task.get('completed'):
+            print ('\t ' +task['title'])
+        # print("\t {}".format(task.get['title']))
