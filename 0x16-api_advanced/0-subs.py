cb9aeaebb8e8c18d95944c5c001a4467d6862f7f
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-15 22:47:21 -0500

    request that brings the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and
+returns the number of subscribers
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that returns the number of
+    subscribers of the reddit API
+    """
+    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    req = requests.get(url, headers={'User-agent': 'url'},
+                       allow_redirects=False).json()
+    try:
+        return (req.get('data').get('subscribers'))
+    except():
+        return 0
