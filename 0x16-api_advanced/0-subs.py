2705a565fddb341b8683855fb2b8ef94f644130c
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-15 22:58:43 -0500

    request that brings the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,8 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers for a given subreddit
     """
     url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
-    headers = {'User-agent': 'url'}
-    req = requests.get(url, headers=headers, allow_redirects=False).json()
+    req = requests.get(url, headers={'User-agent': 'url'},
+                       allow_redirects=False).json()
     try:
         return (req.get('data').get('subscribers'))
     except:
