ec60325868ce53bd5a2b17e3ab901762f75dc359
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-16 18:16:02 -0500

    fixing an extension

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # fixing an extension inside wp-settings.php file
+
 file { '/var/www/html/wp-includes/class-wp-locale.phpp':
     ensure => file,
     source => '/var/www/html/wp-includes/class-wp-locale.php'
