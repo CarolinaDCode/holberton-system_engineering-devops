02e0e008717ac5bcd95c4e3d63e08d9d5118338b
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-16 15:52:09 -0500

    Write a recursive function that queries the Reddit API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
+
+"""unction that queries the Reddit API
+ and returns the number of subscribers"""
+
+import json
 import requests
+from sys import argv
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def count_words(subreddit, word_list, after='', count={}):
+    word_list.sort()
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    cred = {'User-Agent': 'testin'}
+    parameter = {'limit': '100', 'after': after}
+    req = requests.get(url, headers=cred, allow_redirects=False,
+                       params=parameter)
+    if req.status_code != 200:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    else:
+        request_sub = req.json()
+        after = (request_sub.get('data').get('after'))
+        for data in request_sub.get('data').get('children'):
+            for word in word_list:
+                if count.get(word) is None:
+                    count[word] = data.get('data').get('title').count(word) + 0
+                else:
+                    count[word] = data.get('data')\
+                                      .get('title').count(word) \
+                                  + count.get(word)
+    if after is None:
+        for key, value in count.items():
+            if value != 0:
+                print("{}: {:d}".format(key, count[key]))
+        return count
+    return count_words(subreddit, word_list, after, count)
